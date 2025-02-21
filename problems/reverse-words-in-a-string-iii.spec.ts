a802c487b9b1cf14f472a69a6636980d684114af
Emilio Heinzmann <emiliosheinz@gmail.com>
2025-02-21 10:29:18 -0300

    feat: initialize project structure

---
+++ problems/reverse-words-in-a-string-iii.spec.ts
@@ -0,0 +1,8 @@
+import { reverseWords } from "./reverse-words-in-a-string-iii";
+
+describe("reverse-words-in-a-string-iii", () => {
+	test('should correctly reverse the words in "Let\'s take LeetCode contest"', () => {
+		const string = "Let's take LeetCode contest";
+		const result = reverseWords(string);
+	});
+});
