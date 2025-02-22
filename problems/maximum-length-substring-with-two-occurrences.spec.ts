85986c3b431ba40409e81771efc27e71dc84be6b
Emilio Heinzmann <emiliosheinz@gmail.com>
2025-02-22 15:20:12 -0300

    feat: solve maximum-length-substring-with-two-occurrences

---
+++ problems/maximum-length-substring-with-two-occurrences.spec.ts
@@ -0,0 +1,19 @@
+import { maximumLengthSubstring } from "./maximum-length-substring-with-two-occurrences";
+
+type TestData = [string, number]
+
+const testData: TestData[] = [
+	["bcbbbcba", 4],
+	["aaaa", 2],
+  ["aa", 2],
+  ["a", 1]
+];
+
+describe("maximum-length-substring-with-two-occurrences", () => {
+	test.each(testData)(
+		"maximumLengthSubstring(%s) should return %s",
+		(input, output) => {
+			expect(maximumLengthSubstring(input)).toEqual(output);
+		},
+	);
+});
---
+++ problems/maximum-length-substring-with-two-occurrences.ts
@@ -0,0 +1,37 @@
+/**
+ * The solution of this problem involves the concept of sliding window
+ *
+ * @see: https://leetcode.com/problems/maximum-length-substring-with-two-occurrences
+ * */
+export function maximumLengthSubstring(s: string): number {
+  /** Declare initial variables **/
+	let left = 0;
+	let right = 0;
+	let maxLength = 0;
+	const counter: Record<string, number> = {};
+
+  /** Run while the right didn't reach the end */
+	while (right < s.length) {
+    /** Keep track of how many times each character appeared in the string */
+		if (!counter[s[right]]) {
+			counter[s[right]] = 1;
+		} else {
+      counter[s[right]] += 1;
+    }
+
+    /**
+      * If it appeared more than two times (which is the max defined by the problem),
+      * contract the window until the letter is back into the boundaries. 
+      */ 
+    while (counter[s[right]] > 2) {
+      counter[s[left]] -= 1;
+      left++;
+    }
+
+    /** Calculate the new max length */
+    maxLength = Math.max(maxLength, (right - left) + 1);
+		right++;
+	}
+
+	return maxLength;
+}
