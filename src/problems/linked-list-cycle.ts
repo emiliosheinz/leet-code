74a1e3f57469e9a2aeb298ba62d94d1dc4c00d9e
Emilio Heinzmann <emiliosheinz@gmail.com>
2025-04-16 21:11:23 -0300

    feat: implements linked list cycle solution

--- src/problems/linked-list-cycle.ts
+++ src/problems/linked-list-cycle.ts
@@ -1,12 +1,27 @@
 export class ListNode {
-  val: number;
-  next: ListNode | null;
-  constructor(val?: number, next?: ListNode | null) {
-    this.val = val === undefined ? 0 : val;
-    this.next = next === undefined ? null : next;
-  }
+	val: number;
+	next: ListNode | null;
+	constructor(val?: number, next?: ListNode | null) {
+		this.val = val === undefined ? 0 : val;
+		this.next = next === undefined ? null : next;
+	}
 }
 
-export function hasCycle(head: ListNode | null): boolean { 
+/**
+ * This implementation checks if a linked list has a cycle using 
+ * the Floyd's Tortoise and Hare algorithm, aka the two-pointer technique.
+ *
+ * @see https://leetcode.com/problems/linked-list-cycle
+ **/ 
+export function hasCycle(head: ListNode | null): boolean {
+	let slow: ListNode | null = head;
+	let fast: ListNode | null = head;
 
+	while (fast?.next) {
+		slow = slow!.next;
+		fast = fast.next.next;
+		if (slow === fast) return true;
+	}
+
+	return false;
 }
